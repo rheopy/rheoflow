45190d3aff75ceefbd10e1e83f8e3137c7438c8b
William Hartt <hartt.wh@pg.com>
2019-10-14 07:15:48 -0400

    fixed dir structure

---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# Learn more: https://github.com/rheopy/rheoflow
+
+from setuptools import setup, find_packages
+
+readme=''
+licence='Apache 2'
+
+setup(
+    name='rheoflow',
+    version='0.1.0',
+    description='',
+    long_description=readme,
+    author='',
+    author_email='',
+    url='https://github.com/rheopy/rheoflow.git',
+    license=license,
+    install_requires=[],
+    packages=['rheoflow']
+)
